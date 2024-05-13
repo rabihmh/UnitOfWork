b6a75c32aeb2a1dbda86f7e9f3b8e591565bd653
rabihmh <76842351+rabihmh@users.noreply.github.com>
2024-05-13 14:25:36 +0300

    first commit

---
+++ RepositoryPatternWithUOW.EF/UnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿namespace RepositoryPatternWithUOW.EF
+{
+    using RepositoryPatternWithUOW.Core;
+    using RepositoryPatternWithUOW.Core.Interfaces;
+    using RepositoryPatternWithUOW.Core.Models;
+    using RepositoryPatternWithUOW.EF.Repositories;
+
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+
+        public IBaseRepository<Author> Authors { get; private set; }
+        public IBooksRepository Books { get; private set; }
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+
+            Authors = new BaseRepository<Author>(_context);
+            Books = new BooksRepository(_context);
+        }
+
+        public int Complete()
+        {
+            return _context.SaveChanges();
+
+            _context.Model.GetEntityTypes
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
---
+++ RespositoryPatternWithUOW.Api/Program.cs
@@ -0,0 +1,63 @@
+
+namespace RespositoryPatternWithUOW.Api
+{
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.OpenApi.Models;
+    using RepositoryPatternWithUOW.Core;
+    using RepositoryPatternWithUOW.EF;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "authorize", Version = "1" });
+            });
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+
+            builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseCors("all");
+            app.UseStaticFiles();
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
